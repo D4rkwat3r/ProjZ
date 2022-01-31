8171e8b5e80ae64ef8932d25e368502b5c62d07e
darkwater <ktoya170214@gmail.com>
2022-01-31 16:37:39 +0300

    Fixed change password function. Again

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ from .internal.models.message import Message
 from .internal.exceptions.objects.captcha_caught import CaptchaCaught
 from httpx import Client
 
-__version__ = "1.8.2"
+__version__ = "1.8.3"
 
 __latest_version__ = Client().get("https://pypi.org/pypi/ProjZ.py/json").json()["info"]["version"]
 
